b9558fd38d43e94082f96fa3322d938c02862bc1
wmw <wnameless@gmail.com>
2022-12-29 10:43:41 +0800

    Add EnableSpringBootUpMongoDB & SprinBootUpMongoRegistrar

---
+++ src/main/java/com/github/wnameless/spring/boot/up/data/mongodb/EnableSpringBootUpMongoDB.java
@@ -0,0 +1,16 @@
+package com.github.wnameless.spring.boot.up.data.mongodb;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.context.annotation.Import;
+
+@Inherited
+@Documented
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Import(SprinBootUpMongoRegistrar.class)
+public @interface EnableSpringBootUpMongoDB {}
---
+++ src/main/java/com/github/wnameless/spring/boot/up/data/mongodb/SprinBootUpMongoRegistrar.java
@@ -0,0 +1,20 @@
+package com.github.wnameless.spring.boot.up.data.mongodb;
+
+import org.springframework.beans.factory.support.BeanDefinitionRegistry;
+import org.springframework.beans.factory.support.BeanNameGenerator;
+import org.springframework.beans.factory.support.RootBeanDefinition;
+import org.springframework.context.annotation.ImportBeanDefinitionRegistrar;
+import org.springframework.core.type.AnnotationMetadata;
+
+public class SprinBootUpMongoRegistrar implements ImportBeanDefinitionRegistrar {
+
+  @Override
+  public void registerBeanDefinitions(AnnotationMetadata importingClassMetadata,
+      BeanDefinitionRegistry registry, BeanNameGenerator importBeanNameGenerator) {
+    String simpleName = SpringBootUpMongoEventListener.class.getName();
+    RootBeanDefinition rootBeanDefinition = new RootBeanDefinition();
+    rootBeanDefinition.setBeanClass(SpringBootUpMongoEventListener.class);
+    registry.registerBeanDefinition(simpleName, rootBeanDefinition);
+  }
+
+}
