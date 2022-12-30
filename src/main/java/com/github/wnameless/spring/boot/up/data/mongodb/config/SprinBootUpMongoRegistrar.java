21921fecb2f04aa490c3a3157e94744590d64a38
wmw <wnameless@gmail.com>
2022-12-30 21:35:59 +0800

    Remove environment aware interface

--- src/main/java/com/github/wnameless/spring/boot/up/data/mongodb/config/SprinBootUpMongoRegistrar.java
+++ src/main/java/com/github/wnameless/spring/boot/up/data/mongodb/config/SprinBootUpMongoRegistrar.java
@@ -3,16 +3,12 @@ package com.github.wnameless.spring.boot.up.data.mongodb.config;
 import org.springframework.beans.factory.support.BeanDefinitionRegistry;
 import org.springframework.beans.factory.support.BeanNameGenerator;
 import org.springframework.beans.factory.support.GenericBeanDefinition;
-import org.springframework.context.EnvironmentAware;
 import org.springframework.context.annotation.ImportBeanDefinitionRegistrar;
-import org.springframework.core.env.Environment;
 import org.springframework.core.type.AnnotationMetadata;
 import com.github.wnameless.spring.boot.up.data.mongodb.cascade.CascadeMongoEventListener;
 import com.github.wnameless.spring.boot.up.data.mongodb.interceptor.InterceptorMongoEventListener;
 
-public class SprinBootUpMongoRegistrar implements ImportBeanDefinitionRegistrar, EnvironmentAware {
-
-  private Environment environment;
+public class SprinBootUpMongoRegistrar implements ImportBeanDefinitionRegistrar {
 
   @Override
   public void registerBeanDefinitions(AnnotationMetadata importingClassMetadata,
@@ -28,11 +24,6 @@ public class SprinBootUpMongoRegistrar implements ImportBeanDefinitionRegistrar,
     registry.registerBeanDefinition(
         importBeanNameGenerator.generateBeanName(interceptorMongoEventBean, registry),
         interceptorMongoEventBean);
-  }
-
-  @Override
-  public void setEnvironment(Environment environment) {
-    this.environment = environment;
   }
 
 }
