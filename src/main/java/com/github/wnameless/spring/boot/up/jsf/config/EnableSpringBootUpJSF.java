589caf26d13ac271e3fd66ba9089b7ab8c35aa06
wmw <wnameless@gmail.com>
2023-04-14 10:08:36 +0800

    Add EnableSpringBootUpJSF

---
+++ src/main/java/com/github/wnameless/spring/boot/up/jsf/config/EnableSpringBootUpJSF.java
@@ -0,0 +1,16 @@
+package com.github.wnameless.spring.boot.up.jsf.config;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.context.annotation.Import;
+
+@Inherited
+@Documented
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Import({SprinBootUpJSFRegistrar.class})
+public @interface EnableSpringBootUpJSF {}
---
+++ src/main/java/com/github/wnameless/spring/boot/up/jsf/config/SprinBootUpJSFRegistrar.java
@@ -0,0 +1,34 @@
+package com.github.wnameless.spring.boot.up.jsf.config;
+
+import org.springframework.beans.factory.support.BeanDefinitionRegistry;
+import org.springframework.beans.factory.support.BeanNameGenerator;
+import org.springframework.beans.factory.support.GenericBeanDefinition;
+import org.springframework.context.annotation.ImportBeanDefinitionRegistrar;
+import org.springframework.core.type.AnnotationMetadata;
+import org.thymeleaf.dialect.springdata.SpringDataDialect;
+import com.github.wnameless.spring.boot.up.thymeleaf.AjaxDialect;
+import com.github.wnameless.spring.boot.up.thymeleaf.HtmxDialect;
+
+public class SprinBootUpJSFRegistrar implements ImportBeanDefinitionRegistrar {
+
+  @Override
+  public void registerBeanDefinitions(AnnotationMetadata importingClassMetadata,
+      BeanDefinitionRegistry registry, BeanNameGenerator importBeanNameGenerator) {
+    GenericBeanDefinition htmxDialectBean = new GenericBeanDefinition();
+    htmxDialectBean.setBeanClass(HtmxDialect.class);
+    registry.registerBeanDefinition(
+        importBeanNameGenerator.generateBeanName(htmxDialectBean, registry), htmxDialectBean);
+
+    GenericBeanDefinition ajaxDialectBean = new GenericBeanDefinition();
+    ajaxDialectBean.setBeanClass(AjaxDialect.class);
+    registry.registerBeanDefinition(
+        importBeanNameGenerator.generateBeanName(ajaxDialectBean, registry), ajaxDialectBean);
+
+    GenericBeanDefinition springDataDialectBean = new GenericBeanDefinition();
+    htmxDialectBean.setBeanClass(SpringDataDialect.class);
+    registry.registerBeanDefinition(
+        importBeanNameGenerator.generateBeanName(springDataDialectBean, registry),
+        springDataDialectBean);
+  }
+
+}
