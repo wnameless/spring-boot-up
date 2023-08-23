c6d639328d95745d8f4660e2d2da5e964de43f2b
wmw <wnameless@gmail.com>
2023-08-23 16:17:33 +0800

    Add SpringBootUpMongoEventListener

--- src/main/java/com/github/wnameless/spring/boot/up/data/mongodb/config/SprinBootUpMongoRegistrar.java
+++ src/main/java/com/github/wnameless/spring/boot/up/data/mongodb/config/SprinBootUpMongoRegistrar.java
@@ -5,25 +5,17 @@ import org.springframework.beans.factory.support.BeanNameGenerator;
 import org.springframework.beans.factory.support.GenericBeanDefinition;
 import org.springframework.context.annotation.ImportBeanDefinitionRegistrar;
 import org.springframework.core.type.AnnotationMetadata;
-import com.github.wnameless.spring.boot.up.data.mongodb.cascade.CascadeMongoEventListener;
-import com.github.wnameless.spring.boot.up.data.mongodb.interceptor.InterceptorMongoEventListener;
 
 public class SprinBootUpMongoRegistrar implements ImportBeanDefinitionRegistrar {
 
   @Override
   public void registerBeanDefinitions(AnnotationMetadata importingClassMetadata,
       BeanDefinitionRegistry registry, BeanNameGenerator importBeanNameGenerator) {
-    GenericBeanDefinition cascadeMongoEventBean = new GenericBeanDefinition();
-    cascadeMongoEventBean.setBeanClass(CascadeMongoEventListener.class);
+    GenericBeanDefinition springBootUpMongoEventListener = new GenericBeanDefinition();
+    springBootUpMongoEventListener.setBeanClass(SpringBootUpMongoEventListener.class);
     registry.registerBeanDefinition(
-        importBeanNameGenerator.generateBeanName(cascadeMongoEventBean, registry),
-        cascadeMongoEventBean);
-
-    GenericBeanDefinition interceptorMongoEventBean = new GenericBeanDefinition();
-    interceptorMongoEventBean.setBeanClass(InterceptorMongoEventListener.class);
-    registry.registerBeanDefinition(
-        importBeanNameGenerator.generateBeanName(interceptorMongoEventBean, registry),
-        interceptorMongoEventBean);
+        importBeanNameGenerator.generateBeanName(springBootUpMongoEventListener, registry),
+        springBootUpMongoEventListener);
   }
 
 }
