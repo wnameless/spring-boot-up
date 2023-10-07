029c39d18f1233dc441164f94de6700ce9481f7e
wmw <wnameless@gmail.com>
2023-10-07 21:08:57 +0800

    Add #getParam, #findParam

--- src/main/java/com/github/wnameless/spring/boot/up/web/SpringBootUpWeb.java
+++ src/main/java/com/github/wnameless/spring/boot/up/web/SpringBootUpWeb.java
@@ -3,6 +3,7 @@ package com.github.wnameless.spring.boot.up.web;
 import java.util.Optional;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
+import com.github.wnameless.spring.boot.up.SpringBootUp;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
@@ -30,6 +31,26 @@ public final class SpringBootUpWeb {
     return Optional.ofNullable(RequestContextHolder.getRequestAttributes())
         .filter(ServletRequestAttributes.class::isInstance)
         .map(ServletRequestAttributes.class::cast).map(ServletRequestAttributes::getResponse);
+  }
+
+  /**
+   * Returns the first occurance in web request parameters by given parameter name.
+   * 
+   * @param paramName name of the request parameter
+   * @return the first occurance in request parameters, may be null
+   */
+  public static String getParam(String paramName) {
+    return SpringBootUp.getBean(SpringBootUpControllerAdvice.class).getParam(paramName);
+  }
+
+  /**
+   * Finds the first occurance in web request parameters by given parameter name.
+   * 
+   * @param paramName name of the request parameter
+   * @return an {@link Optional} of the first occurance in request parameters
+   */
+  public static Optional<String> findParam(String paramName) {
+    return SpringBootUp.getBean(SpringBootUpControllerAdvice.class).findParam(paramName);
   }
 
 }
