5a1ba4eed074fa46d69429f6d476f48ea2d168d3
wmw <wnameless@gmail.com>
2023-10-07 23:26:41 +0800

    Trim codes

--- src/main/java/com/github/wnameless/spring/boot/up/jsf/JsfPOJO.java
+++ src/main/java/com/github/wnameless/spring/boot/up/jsf/JsfPOJO.java
@@ -12,8 +12,6 @@ import com.github.wnameless.spring.boot.up.SpringBootUp;
 import com.github.wnameless.spring.boot.up.jsf.service.JsfPOJOService;
 import com.github.wnameless.spring.boot.up.jsf.service.JsfPatchService;
 
-
-
 public interface JsfPOJO<T, ID> extends JsonSchemaForm, JsfVersioning, JsfStratrgyAware {
 
   static final Logger log = LoggerFactory.getLogger(JsfPOJO.class);
