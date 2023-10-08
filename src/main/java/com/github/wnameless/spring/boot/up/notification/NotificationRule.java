50eb25f7738f0bb2f25d62b9b9d189a751b6ad61
wmw <wnameless@gmail.com>
2023-10-08 09:26:41 +0800

    Add #getI18nMessage

--- src/main/java/com/github/wnameless/spring/boot/up/notification/NotificationRule.java
+++ src/main/java/com/github/wnameless/spring/boot/up/notification/NotificationRule.java
@@ -3,6 +3,7 @@ package com.github.wnameless.spring.boot.up.notification;
 import com.github.oxo42.stateless4j.delegates.Action1;
 import com.github.oxo42.stateless4j.delegates.Action2;
 import com.github.oxo42.stateless4j.transitions.Transition;
+import com.github.wnameless.spring.boot.up.SpringBootUp;
 import lombok.AccessLevel;
 import lombok.Data;
 import lombok.experimental.FieldDefaults;
@@ -27,6 +28,11 @@ public class NotificationRule<S, T> implements NotificationPlan<S, T> {
     this.trigger = trigger;
     this.state = state;
     this.advice = advice;
+  }
+
+  public String getI18nMessage(Class<?> notificationStrategyType, String suffix) {
+    return SpringBootUp.getMessage(notificationStrategyType.getSimpleName() + "." + trigger + "."
+        + state + "." + advice + "." + suffix);
   }
 
 }
