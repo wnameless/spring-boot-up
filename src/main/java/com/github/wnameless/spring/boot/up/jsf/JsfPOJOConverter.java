bc4cc849eafc8d44e51c7103722fd03dc34f55bf
wmw <wnameless@gmail.com>
2023-10-18 19:36:47 +0800

    Add #beforeMapping, #afterMapping

--- src/main/java/com/github/wnameless/spring/boot/up/jsf/JsfPOJOConverter.java
+++ src/main/java/com/github/wnameless/spring/boot/up/jsf/JsfPOJOConverter.java
@@ -7,11 +7,13 @@ public interface JsfPOJOConverter<P, J extends JsfPOJO<P, ?>> extends MapModelCo
 
   @Override
   default void map(P source, J target) {
+    if (beforeMapping() != null) beforeMapping().accept(target);
     J converted = convert(source);
     ModelMapper modelMapper = JsfConfig.getModelMapper();
     P pojo = target.getPojo();
     modelMapper.map(converted, target);
     target.setPojo(pojo);
+    if (afterMapping() != null) afterMapping().accept(target);
   }
 
 }
--- src/main/java/com/github/wnameless/spring/boot/up/model/MapModelConverter.java
+++ src/main/java/com/github/wnameless/spring/boot/up/model/MapModelConverter.java
@@ -1,14 +1,25 @@
 package com.github.wnameless.spring.boot.up.model;
 
+import java.util.function.Consumer;
 import org.modelmapper.ModelMapper;
 import org.springframework.core.convert.converter.Converter;
 
 public interface MapModelConverter<S, T> extends Converter<S, T> {
 
+  default Consumer<T> beforeMapping() {
+    return null;
+  }
+
   default void map(S source, T target) {
+    if (beforeMapping() != null) beforeMapping().accept(target);
     T converted = convert(source);
     ModelMapper modelMapper = ModelConfig.getModelMapper();
     modelMapper.map(converted, target);
+    if (afterMapping() != null) afterMapping().accept(target);
+  }
+
+  default Consumer<T> afterMapping() {
+    return null;
   }
 
 }
