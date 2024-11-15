5d45dc7d26f35c58698da0f3a26f94c3e0d42681
wmw <wnameless@gmail.com>
2024-11-15 15:40:24 +0800

    Decode name in Base64EncodingAttachmentableData

--- src/main/java/com/github/wnameless/spring/boot/up/attachment/Base64EncodingAttachmentableData.java
+++ src/main/java/com/github/wnameless/spring/boot/up/attachment/Base64EncodingAttachmentableData.java
@@ -1,5 +1,8 @@
 package com.github.wnameless.spring.boot.up.attachment;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
 import java.util.regex.Pattern;
 import lombok.AccessLevel;
 import lombok.Data;
@@ -19,7 +22,13 @@ public class Base64EncodingAttachmentableData implements AttachmentableData {
     String[] parts = rjsfBase64Data.split(Pattern.quote(";"));
 
     mimeType = parts[0].split(Pattern.quote(":"))[1];
-    name = parts[1].split(Pattern.quote("="))[1];
+    // name = parts[1].split(Pattern.quote("="))[1];
+    try {
+      name =
+          URLDecoder.decode(parts[1].split(Pattern.quote("="))[1], StandardCharsets.UTF_8.name());
+    } catch (UnsupportedEncodingException e) {
+      name = parts[1].split(Pattern.quote("="))[1];
+    }
     data = parts[2].split(Pattern.quote(","))[1];
   }
 
