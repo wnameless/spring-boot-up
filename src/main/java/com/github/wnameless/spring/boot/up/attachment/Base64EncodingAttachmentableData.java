55e97030d5d69d91252a74ad9a10325a273d6d17
wmw <wnameless@gmail.com>
2024-12-08 15:17:08 +0800

    Enhence Base64EncodingAttachmentableData

--- src/main/java/com/github/wnameless/spring/boot/up/attachment/Base64EncodingAttachmentableData.java
+++ src/main/java/com/github/wnameless/spring/boot/up/attachment/Base64EncodingAttachmentableData.java
@@ -2,39 +2,54 @@ package com.github.wnameless.spring.boot.up.attachment;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
+import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 import java.util.regex.Pattern;
 import lombok.AccessLevel;
 import lombok.Data;
+import lombok.SneakyThrows;
 import lombok.experimental.FieldDefaults;
 
 @Data
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class Base64EncodingAttachmentableData implements AttachmentableData {
 
-  String mimeType;
+  final String mimeType;
 
-  String name;
+  final String name;
 
-  String data;
+  final String data;
+
+  public Base64EncodingAttachmentableData(String mimeType, String name, String data) {
+    this.mimeType = mimeType;
+    this.name = name;
+    this.data = data;
+  }
 
   public Base64EncodingAttachmentableData(String rjsfBase64Data) {
     String[] parts = rjsfBase64Data.split(Pattern.quote(";"));
 
     mimeType = parts[0].split(Pattern.quote(":"))[1];
-    // name = parts[1].split(Pattern.quote("="))[1];
+    String name = parts[1].split(Pattern.quote("="))[1];
     try {
       name =
           URLDecoder.decode(parts[1].split(Pattern.quote("="))[1], StandardCharsets.UTF_8.name());
     } catch (UnsupportedEncodingException e) {
       name = parts[1].split(Pattern.quote("="))[1];
     }
+    this.name = name;
     data = parts[2].split(Pattern.quote(","))[1];
   }
 
   @Override
   public byte[] getBytes() {
     return data.getBytes();
+  }
+
+  @SneakyThrows
+  public String toBase64Data() {
+    return "data:" + mimeType + ";" + "name="
+        + URLEncoder.encode(name, StandardCharsets.UTF_8.name()) + ";base64," + data;
   }
 
 }
