6879d61404a19893a10323c59aaa26d28952518e
wmw <wnameless@gmail.com>
2024-12-24 07:48:01 +0800

    Deploy 0.10.3-SNAPSHOT

--- src/main/java/com/github/wnameless/spring/boot/up/fsm/StateFormAdvice.java
+++ src/main/java/com/github/wnameless/spring/boot/up/fsm/StateFormAdvice.java
@@ -2,14 +2,25 @@ package com.github.wnameless.spring.boot.up.fsm;
 
 import java.util.function.BiFunction;
 import java.util.function.BooleanSupplier;
+import org.springframework.core.GenericTypeResolver;
 import com.github.wnameless.spring.boot.up.jsf.JsfVersioning;
 import com.github.wnameless.spring.boot.up.jsf.JsonSchemaForm;
 
 public interface StateFormAdvice<SF extends JsonSchemaForm & JsfVersioning, PA extends PhaseProvider<?, ?, ?, ?>> {
 
-  Class<SF> getStateFormType();
+  @SuppressWarnings("unchecked")
+  default Class<SF> getStateFormType() {
+    var genericTypeResolver =
+        GenericTypeResolver.resolveTypeArguments(this.getClass(), StateFormAdvice.class);
+    return (Class<SF>) genericTypeResolver[0];
+  }
 
-  Class<PA> getPhaseAwareType();
+  @SuppressWarnings("unchecked")
+  default Class<PA> getPhaseAwareType() {
+    var genericTypeResolver =
+        GenericTypeResolver.resolveTypeArguments(this.getClass(), StateFormAdvice.class);
+    return (Class<PA>) genericTypeResolver[1];
+  }
 
   default int getOrder() {
     return 0;
