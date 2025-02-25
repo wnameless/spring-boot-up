efe3b379202969a5651cfa33e09e5cbbfcea6104
wmw <wnameless@gmail.com>
2025-02-25 16:34:05 +0800

    Improve WebActionAlertHelper

--- src/main/java/com/github/wnameless/spring/boot/up/web/WebActionAlertHelper.java
+++ src/main/java/com/github/wnameless/spring/boot/up/web/WebActionAlertHelper.java
@@ -3,6 +3,7 @@ package com.github.wnameless.spring.boot.up.web;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.util.ClassUtils;
 import org.springframework.web.servlet.ModelAndView;
 import com.github.wnameless.spring.boot.up.SpringBootUp;
@@ -11,13 +12,13 @@ import lombok.Data;
 
 public interface WebActionAlertHelper {
 
-  default void alert(String action) {
+  default void alert(String action, Object... args) {
     addAlertMessages(SpringBootUp.getBean(SpringBootUpControllerAdvice.class).getModelAndView(),
-        action);
+        action, args);
   }
 
-  default void alert(String action, ModelAndView mav) {
-    addAlertMessages(mav, action);
+  default void alert(String action, ModelAndView mav, Object... args) {
+    addAlertMessages(mav, action, args);
   }
 
   @Data
@@ -30,25 +31,26 @@ public interface WebActionAlertHelper {
 
   }
 
-  default void addAlertMessages(ModelAndView mav, String action) {
+  default void addAlertMessages(ModelAndView mav, String action, Object... args) {
     String controllerSimpleName = ClassUtils.getUserClass(getClass()).getSimpleName();
     String dangerKey = "controller." + controllerSimpleName + "." + action + ".alert.danger";
     String warningKey = "controller." + controllerSimpleName + "." + action + ".alert.warning";
     String infoKey = "controller." + controllerSimpleName + "." + action + ".alert.info";
     String successKey = "controller." + controllerSimpleName + "." + action + ".alert.success";
 
-    var alert = new AlertMessages();
-    SpringBootUp.findMessage(dangerKey).ifPresent(message -> {
-      alert.setDanger(Arrays.asList(message.split(",")));
+    var alert = Optional.ofNullable((AlertMessages) mav.getModel().get(Alert.name()))
+        .orElse(new AlertMessages());
+    SpringBootUp.findMessage(dangerKey, args).ifPresent(message -> {
+      alert.getDanger().addAll(Arrays.asList(message.split(",")));
     });
-    SpringBootUp.findMessage(warningKey).ifPresent(message -> {
-      alert.setWarning(Arrays.asList(message.split(",")));
+    SpringBootUp.findMessage(warningKey, args).ifPresent(message -> {
+      alert.getWarning().addAll(Arrays.asList(message.split(",")));
     });
-    SpringBootUp.findMessage(infoKey).ifPresent(message -> {
-      alert.setInfo(Arrays.asList(message.split(",")));
+    SpringBootUp.findMessage(infoKey, args).ifPresent(message -> {
+      alert.getInfo().addAll(Arrays.asList(message.split(",")));
     });
-    SpringBootUp.findMessage(successKey).ifPresent(message -> {
-      alert.setSuccess(Arrays.asList(message.split(",")));
+    SpringBootUp.findMessage(successKey, args).ifPresent(message -> {
+      alert.getSuccess().addAll(Arrays.asList(message.split(",")));
     });
     mav.addObject(Alert.name(), alert);
   }
