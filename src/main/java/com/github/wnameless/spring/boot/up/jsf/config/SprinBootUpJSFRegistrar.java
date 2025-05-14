6c9bf57fd72829bec8b3558c54c8b0fa63942d35
wmw <wnameless@gmail.com>
2025-05-14 15:55:14 +0800

    Add @NonNull

--- src/main/java/com/github/wnameless/spring/boot/up/jsf/config/SprinBootUpJSFRegistrar.java
+++ src/main/java/com/github/wnameless/spring/boot/up/jsf/config/SprinBootUpJSFRegistrar.java
@@ -5,14 +5,16 @@ import org.springframework.beans.factory.support.BeanNameGenerator;
 import org.springframework.beans.factory.support.GenericBeanDefinition;
 import org.springframework.context.annotation.ImportBeanDefinitionRegistrar;
 import org.springframework.core.type.AnnotationMetadata;
+import org.springframework.lang.NonNull;
 import org.thymeleaf.dialect.springdata.SpringDataDialect;
 import com.github.wnameless.spring.boot.up.thymeleaf.AjaxDialect;
 
 public class SprinBootUpJSFRegistrar implements ImportBeanDefinitionRegistrar {
 
   @Override
-  public void registerBeanDefinitions(AnnotationMetadata importingClassMetadata,
-      BeanDefinitionRegistry registry, BeanNameGenerator importBeanNameGenerator) {
+  public void registerBeanDefinitions(@NonNull AnnotationMetadata importingClassMetadata,
+      @NonNull BeanDefinitionRegistry registry,
+      @NonNull BeanNameGenerator importBeanNameGenerator) {
     GenericBeanDefinition ajaxDialectBean = new GenericBeanDefinition();
     ajaxDialectBean.setBeanClass(AjaxDialect.class);
     registry.registerBeanDefinition(
