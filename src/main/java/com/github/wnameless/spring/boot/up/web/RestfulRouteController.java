9e7748e0028a4b2f1a52594f0684d921a1df9087
wmw <wnameless@gmail.com>
2025-06-04 07:34:46 +0800

    Add RestfulRouteController#templateFragment and _historyBack

--- src/main/java/com/github/wnameless/spring/boot/up/web/RestfulRouteController.java
+++ src/main/java/com/github/wnameless/spring/boot/up/web/RestfulRouteController.java
@@ -1,5 +1,6 @@
 package com.github.wnameless.spring.boot.up.web;
 
+import org.springframework.http.MediaType;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import com.github.wnameless.spring.boot.up.web.ModelAttributes.Route;
@@ -27,6 +28,16 @@ public interface RestfulRouteController<ID> extends WebActionAlertHelper, Restfu
   @ModelAttribute
   default void setTemplateRoute(Model model) {
     model.addAttribute(getTemplateRouteKey(), getTemplateRoute());
+  }
+
+  default String templateFragment() {
+    var reqOpt = SpringBootUpWeb.findHttpServletRequest();
+    if (reqOpt.isPresent()) {
+      if (reqOpt.get().getContentType() instanceof String contentType) {
+        if (contentType.startsWith(MediaType.APPLICATION_JSON_VALUE)) return "partial";
+      }
+    }
+    return "complete";
   }
 
 }
