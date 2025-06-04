975208d3a4757f5610b71ad6345bf3441a9264ab
wmw <wnameless@gmail.com>
2025-06-05 05:05:16 +0800

    Add attachment dialog

--- src/main/java/com/github/wnameless/spring/boot/up/attachment/Attachment.java
+++ src/main/java/com/github/wnameless/spring/boot/up/attachment/Attachment.java
@@ -3,6 +3,7 @@ package com.github.wnameless.spring.boot.up.attachment;
 import java.net.URI;
 import java.time.LocalDateTime;
 import com.github.wnameless.spring.boot.up.web.IdProvider;
+import com.google.common.base.Strings;
 
 public interface Attachment<ID> extends IdProvider<ID> {
 
@@ -27,5 +28,10 @@ public interface Attachment<ID> extends IdProvider<ID> {
   LocalDateTime getCreatedAt();
 
   void setCreatedAt(LocalDateTime createdAt);
+
+  default String getNameWithNote() {
+    String note = Strings.nullToEmpty(getNote());
+    return "" + getName() + (note.isBlank() ? "" : " - " + note);
+  }
 
 }
