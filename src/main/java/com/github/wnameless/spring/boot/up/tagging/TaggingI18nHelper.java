da079babc2f475db044547767feec31dd0594a17
wmw <wnameless@gmail.com>
2025-06-10 06:03:28 +0800

    Make TaggingI18nHelper i18n default Eng

--- src/main/java/com/github/wnameless/spring/boot/up/tagging/TaggingI18nHelper.java
+++ src/main/java/com/github/wnameless/spring/boot/up/tagging/TaggingI18nHelper.java
@@ -5,19 +5,19 @@ import com.github.wnameless.spring.boot.up.SpringBootUp;
 public class TaggingI18nHelper {
 
   public static String getTaggingTitle() {
-    return SpringBootUp.getMessage("sbu.tagging.title", null, "註記標籤");
+    return SpringBootUp.getMessage("sbu.tagging.panel.tagging", null, "Tagging Labels");
   }
 
   public static String getLabelListName() {
-    return SpringBootUp.getMessage("sbu.tagging.labelList.title", null, "公用標籤");
+    return SpringBootUp.getMessage("sbu.tagging.panel.labelList", null, "Public Labels");
   }
 
   public static String getUserLabelListName() {
-    return SpringBootUp.getMessage("sbu.tagging.userLabelList.title", null, "私人標籤");
+    return SpringBootUp.getMessage("sbu.tagging.panel.userLabelList", null, "Private Labels");
   }
 
   public static String getSystemLabelListName() {
-    return SpringBootUp.getMessage("sbu.tagging.systemLabelList.title", null, "系統標籤");
+    return SpringBootUp.getMessage("sbu.tagging.panel.systemLabelList", null, "System Labels");
   }
 
 }
