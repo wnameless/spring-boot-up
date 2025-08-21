2ca3ce3bf33b9c1689951e633e871cecf287764b
wmw <wnameless@gmail.com>
2025-08-22 02:47:12 +0800

    Update JsfDataUtils

--- src/main/java/com/github/wnameless/spring/boot/up/jsf/util/JsfDataUtils.java
+++ src/main/java/com/github/wnameless/spring/boot/up/jsf/util/JsfDataUtils.java
@@ -13,33 +13,47 @@ public class JsfDataUtils {
 
   public Map<String, Object> mergeData(Map<String, Object> data,
       Map<String, Object> additionalData) {
-    return mergeSchema(data, additionalData);
+    return mergeData(data, additionalData, false);
+  }
+
+  public Map<String, Object> mergeData(Map<String, Object> data,
+      Map<String, Object> additionalData, boolean overrideArray) {
+    return mergeSchema(data, additionalData, overrideArray);
   }
 
   public Map<String, Object> mergeSchema(Map<String, Object> schema,
       Map<String, Object> additionalData) {
+    return mergeSchema(schema, additionalData, false);
+  }
+
+  public Map<String, Object> mergeSchema(Map<String, Object> schema,
+      Map<String, Object> additionalData, boolean overrideArray) {
     var mapperOpt = SpringBootUp.findBean(ObjectMapper.class);
     ObjectMapper mapper = mapperOpt.orElse(new ObjectMapper());
     ObjectNode baseNode = mapper.convertValue(schema, ObjectNode.class);
     ObjectNode overrideNode = mapper.convertValue(additionalData, ObjectNode.class);
 
-    merge(baseNode, overrideNode);
+    merge(baseNode, overrideNode, overrideArray);
 
     return mapper.convertValue(baseNode, new TypeReference<Map<String, Object>>() {});
   }
 
-  private static void merge(ObjectNode baseNode, ObjectNode overrideNode) {
+  private static void merge(ObjectNode baseNode, ObjectNode overrideNode, boolean overrideArray) {
     overrideNode.fieldNames().forEachRemaining(field -> {
       var baseValue = baseNode.get(field);
       var overrideValue = overrideNode.get(field);
 
       if (baseValue != null && baseValue.isObject() && overrideValue.isObject()) {
-        merge((ObjectNode) baseValue, (ObjectNode) overrideValue);
+        merge((ObjectNode) baseValue, (ObjectNode) overrideValue, overrideArray);
       } else if (baseValue != null && baseValue.isArray() && overrideValue.isArray()) {
-        ArrayNode mergedArray = ((ObjectNode) baseNode).arrayNode();
-        mergedArray.addAll((ArrayNode) baseValue);
-        mergedArray.addAll((ArrayNode) overrideValue);
-        baseNode.set(field, mergedArray);
+        if (overrideArray) {
+          baseNode.set(field, overrideValue);
+        } else {
+          ArrayNode mergedArray = ((ObjectNode) baseNode).arrayNode();
+          mergedArray.addAll((ArrayNode) baseValue);
+          mergedArray.addAll((ArrayNode) overrideValue);
+          baseNode.set(field, mergedArray);
+        }
       } else {
         baseNode.set(field, overrideValue);
       }
